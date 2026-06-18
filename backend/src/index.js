5f329b813942f27c82cf1726cdd7268d7b4147a3
Frank <ashishfrank1234@gmail.com>
2026-06-18 14:48:51 +0530

    Add user and message models, database connection, and integrate Clerk middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,22 @@
 import express from 'express'
 import 'dotenv/config'
+import { connectDB } from './lib/db.js'
+import { clerkMiddleware } from '@clerk/express'
+import cors from'cors'
 
 const app = express()
 const PORT = process.env.PORT
+const FRONTEND_URL = process.env.FRONTEND_URL
+
+app.use(express.json())
+app.use(cors({ origin: FRONTEND_URL, credentials: true }))
+app.use(clerkMiddleware())
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ message: 'Healthy' })
+})
 
 app.listen(PORT, () => {
+    connectDB()
     console.log(`server running on PORT ${PORT}`)
 })
